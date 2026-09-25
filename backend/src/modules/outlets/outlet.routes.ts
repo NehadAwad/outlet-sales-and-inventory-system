6c55bf61821edd3af3878a2da1302b9d816ffe87
NehadAwad <nehadakash622@gmail.com>
2026-09-25 11:06:54 +0600

    sales with receipt numbers

--- backend/src/modules/outlets/outlet.routes.ts
+++ backend/src/modules/outlets/outlet.routes.ts
@@ -3,6 +3,7 @@ import { validate } from "../../middlewares/validate.middleware";
 import { asyncHandler } from "../../utils/asyncHandler";
 import { inventoryRoutes } from "../inventory/inventory.routes";
 import { outletMenuRoutes } from "../outlet-menu/outletMenu.routes";
+import { saleRoutes } from "../sales/sale.routes";
 import * as outletController from "./outlet.controller";
 import {
   createOutletSchema,
@@ -23,6 +24,8 @@ outletRoutes.get("/", asyncHandler(outletController.listOutlets));
 outletRoutes.use("/:outletId/menu-items", outletMenuRoutes);
 
 outletRoutes.use("/:outletId/inventory", inventoryRoutes);
+
+outletRoutes.use("/:outletId/sales", saleRoutes);
 
 outletRoutes.get(
   "/:outletId",
